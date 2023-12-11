27b59c34e2e58735cf276996245b1ed842736715
Xi Chen <charlchen1018@gmail.com>
2023-12-11 13:48:24 -0500

    add warning for invalid date

--- src/DateDifferenceCalculator.js
+++ src/DateDifferenceCalculator.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { differenceInCalendarMonths, differenceInCalendarYears } from 'date-fns';
 
 const DateDifferenceCalculator = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [difference, setDifference] = useState(null);
+  const [error, setError] = useState('');
 
   const handleDateChange = (event, type) => {
     const newDate = new Date(event.target.value);
@@ -14,15 +16,31 @@ const DateDifferenceCalculator = () => {
     }
   };
 
-  const calculateDifference = () => {
+  useEffect(() => {
+    const calculateDifference = () => {
+      if (startDate && endDate && startDate <= endDate) {
+        const diffInSeconds = Math.floor((endDate - startDate) / 1000);
+        setDifference(diffInSeconds);
+        setError('');
+      } else {
+        setDifference(null);
+        setError('End date must be equal to or later than the start date.');
+      }
+    };
+
+    calculateDifference();
+  }, [startDate, endDate]);
+
+  const calculateMonthsAndYears = () => {
     if (startDate && endDate && startDate <= endDate) {
-      const diffInMillis = endDate - startDate;
-      const diffInSeconds = Math.floor(diffInMillis / 1000);
-      setDifference(diffInSeconds);
-    } else {
-      setDifference(null);
+      const monthsDifference = differenceInCalendarMonths(endDate, startDate);
+      const yearsDifference = differenceInCalendarYears(endDate, startDate);
+      return { months: monthsDifference, years: yearsDifference };
     }
+    return { months: 0, years: 0 };
   };
+
+  const { months, years } = calculateMonthsAndYears();
 
   return (
     <div>
@@ -37,14 +55,15 @@ const DateDifferenceCalculator = () => {
           <input type="datetime-local" value={endDate.toISOString().slice(0, 16)} onChange={(e) => handleDateChange(e, 'end')} />
         </label>
       </form>
-      <button onClick={calculateDifference}>Calculate Difference</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       {difference !== null && (
         <div>
           <p>Difference in seconds: {difference}s</p>
           <p>Difference in minutes: {Math.floor(difference / 60)}m</p>
           <p>Difference in hours: {Math.floor(difference / 3600)}h</p>
           <p>Difference in days: {Math.floor(difference / 86400)}d</p>
-          {/* Additional calculations for months and years can be added here */}
+          <p>Difference in months: {months} months</p>
+          <p>Difference in years: {years} years</p>
         </div>
       )}
     </div>
